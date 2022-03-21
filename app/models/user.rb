e5d0b3308a213e9592c961a5b2a6b00cf7044391
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-03-21 18:24:04 -0300

    update validation for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: { in: 5..20 }, uniqueness: true
-  validates :email, presence: true, uniqueness: true, length: { in: 5..20 }
+  validates :email, presence: true, uniqueness: true, length: { in: 5..40 }
   validates :password, presence: true, uniqueness: true, length: { minimum: 8 }
 end
